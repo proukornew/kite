35756eb72387fac150eaa6f01bb8c477bf324ee1
andrew.fedoniouk@gmail.com <andrew.fedoniouk@gmail.com@70d6cab3-1967-365b-858a-5bf25f3bc9a7>
2011-03-08 03:48:45 +0000

    added KiTE home reference

--- trunk/kite.js
+++ trunk/kite.js
@@ -20,6 +20,7 @@
   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
   
+ // KiTE home: http://code.google.com/p/kite/
 
 (kite = function(template, data, formatters)
   {
